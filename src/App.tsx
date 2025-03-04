5b4eae049d8bf3c0bd20f149b8f193b1b47f7297
alexischebeste <alexispaulino.chebeste@estudiantes.unahur.edu.ar>
2025-03-04 01:11:55 -0300

    galery section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar"
 import Event from "./components/Event"
 import Schedules from "./components/Schedules"
 import Footer from "./components/Footer"
+import Galery from "./components/Galery"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Favorites />
       <Curiosity />
       <Event />
+      <Galery />
       <Schedules />
       <Footer />
     </main>
